0f2b5bc67f648c8c798c7c4b990394ebf15d1b2f
harishumesh <harish.maddy3@gmail.com>
2022-06-19 10:08:13 +0530

    Jun 19 GIT work

---
+++ Git_19June/src/Pgm_01_GIT.java
@@ -0,0 +1,11 @@
+
+public class Pgm_01_GIT {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Welcome to GIT repositry-19June");
+
+	}
+
+}
